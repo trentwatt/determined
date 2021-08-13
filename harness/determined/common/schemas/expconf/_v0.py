0dd2d1eda58c56832c6e5547031af35f90d75310
Ilia Glazkov <ilia.glazkov@hpe.com>
2021-08-13 15:54:42 -0700

    bumpenvs

--- harness/determined/common/schemas/expconf/_v0.py
+++ harness/determined/common/schemas/expconf/_v0.py
@@ -246,7 +246,7 @@ class EnvironmentImageV0(schemas.SchemaBase):
             )
         if self.gpu is None:
             self.gpu = (
-                "determinedai/environments:cuda-11.1-pytorch-1.9-lightning-1.3-tf-2.4-gpu-2409e48"
+                "determinedilia/environments:cuda-11.1-pytorch-1.9-lightning-1.3-tf-2.4-gpu-b72299a"
             )
 
 
--- master/pkg/model/defaults.go
+++ master/pkg/model/defaults.go
@@ -26,7 +26,7 @@ const (
 // Default task environment docker image names.
 const (
 	CPUImage = "determinedai/environments:py-3.7-pytorch-1.9-lightning-1.3-tf-2.4-cpu-2409e48"
-	GPUImage = "determinedai/environments:cuda-11.1-pytorch-1.9-lightning-1.3-tf-2.4-gpu-2409e48"
+	GPUImage = "determinedilia/environments:cuda-11.1-pytorch-1.9-lightning-1.3-tf-2.4-gpu-b72299a"
 )
 
 // DefaultResourcesConfig returns the default resources configuration.
