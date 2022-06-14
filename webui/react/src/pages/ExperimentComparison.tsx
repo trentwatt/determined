7cd5d26d3a031d742a2214b70d2884efd28ee1bd
julian-determined-ai <julian.glover@hpe.com>
2022-06-14 14:28:24 -0500

    begin working on multi exp comparison component

--- webui/react/src/pages/ExperimentComparison.tsx
+++ webui/react/src/pages/ExperimentComparison.tsx
@@ -1,27 +1,24 @@
-// import React, { useCallback, useEffect, useRef, useState } from 'react';
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useLocation } from 'react-router';
 import queryString from 'query-string';
+import Page from 'components/Page';
+import { terminalRunStates } from 'constants/states';
+import { useStore } from 'contexts/Store';
+import usePolling from 'hooks/usePolling';
+import ExperimentDetailsHeader from 'pages/ExperimentDetails/ExperimentDetailsHeader';
+import {
+  getExperimentDetails, getExpValidationHistory, isNotFound,
+} from 'services/api';
+import Message, { MessageType } from 'shared/components/Message';
+import Spinner from 'shared/components/Spinner/Spinner';
+import { isEqual } from 'shared/utils/data';
+import { ExperimentBase, TrialDetails, ValidationHistory } from 'types';
+import { isSingleTrialExperiment } from 'utils/experiment';
 
-import Message from 'shared/components/Message';
-// import Page from 'components/Page';
-// import { terminalRunStates } from 'constants/states';
-// import { useStore } from 'contexts/Store';
-// import usePolling from 'hooks/usePolling';
-// import ExperimentDetailsHeader from 'pages/ExperimentDetails/ExperimentDetailsHeader';
-// import {
-//   getExperimentDetails, getExpValidationHistory, isNotFound,
-// } from 'services/api';
-// import Message, { MessageType } from 'shared/components/Message';
-// import Spinner from 'shared/components/Spinner/Spinner';
-// import { isEqual } from 'shared/utils/data';
-// import { ExperimentBase, TrialDetails, ValidationHistory } from 'types';
-// import { isSingleTrialExperiment } from 'utils/experiment';
+import { isAborted } from '../shared/utils/service';
 
-// import { isAborted } from '../shared/utils/service';
-
-// import ExperimentMultiTrialTabs from './ExperimentDetails/ExperimentMultiTrialTabs';
-// import ExperimentSingleTrialTabs from './ExperimentDetails/ExperimentSingleTrialTabs';
+import ExperimentMultiTrialTabs from './ExperimentDetails/ExperimentMultiTrialTabs';
+import ExperimentSingleTrialTabs from './ExperimentDetails/ExperimentSingleTrialTabs';
 
 interface Query {
   id?: string[];
@@ -30,100 +27,77 @@ interface Query {
 const ExperimentComparison: React.FC = () => {
   const location = useLocation();
   const query: Query = queryString.parse(location.search);
-  const experimentIds = query.id;
+  const experimentIds = query.id ? query.id : [];
   
-  // const { auth: { user } } = useStore();
-  // const [ canceler ] = useState(new AbortController());
-  // const [ experiment, setExperiment ] = useState<ExperimentBase>();
-  // const [ trial, setTrial ] = useState<TrialDetails>();
-  // const [ valHistory, setValHistory ] = useState<ValidationHistory[]>([]);
-  // const [ pageError, setPageError ] = useState<Error>();
-  // const [ isSingleTrial, setIsSingleTrial ] = useState<boolean>();
-  // const pageRef = useRef<HTMLElement>(null);
+  const { auth: { user } } = useStore();
+  const [ canceler ] = useState(new AbortController());
+  const [ experiment, setExperiment ] = useState<ExperimentBase>();
+  const [ trial, setTrial ] = useState<TrialDetails>();
+  const [ valHistory, setValHistory ] = useState<ValidationHistory[]>([]);
+  const [ pageError, setPageError ] = useState<Error>();
+  const [ isSingleTrial, setIsSingleTrial ] = useState<boolean>();
+  const pageRef = useRef<HTMLElement>(null);
 
-  // just testing ID parsing
+  const fetchExperimentDetails = useCallback(async () => {
+    try {
+      const [ experimentData ] = await Promise.all(
+        experimentIds.map(id => getExperimentDetails({id: parseInt(id)}, { signal: canceler.signal }))
+      );
+      if (!isEqual(experimentData, experiment)) setExperiment(experimentData);
+    } catch (e) {
+      if (!pageError && !isAborted(e)) setPageError(e as Error);
+    }
+  }, [
+    experiment,
+    experimentIds,
+    canceler.signal,
+    pageError,
+    valHistory,
+  ]);
 
-  // const fetchExperimentDetails = useCallback(async () => {
-  //   try {
-  //     const [ experimentData, validationHistory ] = await Promise.all([
-  //       getExperimentDetails({ id }, { signal: canceler.signal }),
-  //       getExpValidationHistory({ id }, { signal: canceler.signal }),
-  //     ]);
-  //     if (!isEqual(experimentData, experiment)) setExperiment(experimentData);
-  //     if (!isEqual(validationHistory, valHistory)) setValHistory(validationHistory);
-  //     setIsSingleTrial(
-  //       isSingleTrialExperiment(experimentData),
-  //     );
-  //   } catch (e) {
-  //     if (!pageError && !isAborted(e)) setPageError(e as Error);
-  //   }
-  // }, [
-  //   experiment,
-  //   id,
-  //   canceler.signal,
-  //   pageError,
-  //   valHistory,
-  // ]);
+  const { stopPolling } = usePolling(fetchExperimentDetails);
 
-  // const { stopPolling } = usePolling(fetchExperimentDetails);
+  const handleSingleTrialUpdate = useCallback((trial: TrialDetails) => {
+    setTrial(trial);
+  }, []);
 
-  // const handleSingleTrialUpdate = useCallback((trial: TrialDetails) => {
-  //   setTrial(trial);
-  // }, []);
+  useEffect(() => {
+    if (experiment && terminalRunStates.has(experiment.state)) {
+      stopPolling();
+    }
+  }, [ experiment, stopPolling ]);
 
-  // useEffect(() => {
-  //   if (experiment && terminalRunStates.has(experiment.state)) {
-  //     stopPolling();
-  //   }
-  // }, [ experiment, stopPolling ]);
+  useEffect(() => {
+    return () => canceler.abort();
+  }, [ canceler ]);
 
-  // useEffect(() => {
-  //   return () => canceler.abort();
-  // }, [ canceler ]);
+  if (!experimentIds) {
+    return <Message title='No Experiments chosen for comparison'/>;
+  } else if (pageError) {
+    return <Message title="Unable to compare experiments" type={MessageType.Warning} />;
+  } 
 
-  // if (isNaN(id)) {
-  //   return <Message title={`Invalid Experiment ID ${experimentId}`} />;
-  // } else if (pageError) {
-  //   const message = isNotFound(pageError) ?
-  //     `Unable to find Experiment ${experimentId}` :
-  //     `Unable to fetch Experiment ${experimentId}`;
-  //   return <Message title={message} type={MessageType.Warning} />;
-  // } else if (!experiment || isSingleTrial === undefined) {
-  //   return <Spinner tip={`Loading experiment ${experimentId} details...`} />;
-  // }
-
-  return <Message title={`Showing Experiment Ids ${experimentIds}`} />;
-
-  // return (
-  //   <Page
-  //     bodyNoPadding
-  //     containerRef={pageRef}
-  //     headerComponent={(
-  //       <ExperimentDetailsHeader
-  //         curUser={user}
-  //         experiment={experiment}
-  //         fetchExperimentDetails={fetchExperimentDetails}
-  //         trial={trial}
-  //       />
-  //     )}
-  //     stickyHeader
-  //     title={`Experiment ${experimentId}`}>
-  //     {isSingleTrial ? (
-  //       <ExperimentSingleTrialTabs
-  //         experiment={experiment}
-  //         fetchExperimentDetails={fetchExperimentDetails}
-  //         pageRef={pageRef}
-  //         onTrialUpdate={handleSingleTrialUpdate}
-  //       />
-  //     ) : (
-  //       <ExperimentMultiTrialTabs
-  //         experiment={experiment}
-  //         fetchExperimentDetails={fetchExperimentDetails}
-  //         pageRef={pageRef}
-  //       />
-  //     )}
-  //   </Page>
-  // );
+  return (
+    <Page
+      bodyNoPadding
+      containerRef={pageRef}
+      // headerComponent={(
+      //   <ExperimentDetailsHeader
+      //     curUser={user}
+      //     experiment={experiment}
+      //     fetchExperimentDetails={fetchExperimentDetails}
+      //     trial={trial}
+      //   />
+      // )}
+      stickyHeader
+      title="Compare Experiments">
+        {/* <ExperimentMultiTrialTabs
+          experiment={experiment}
+          fetchExperimentDetails={fetchExperimentDetails}
+          pageRef={pageRef}
+        /> */}
+    </Page>
+  );
 };
 
 export default ExperimentComparison;
