667815ddb3cf1de2106dc1791e599991b4c77bbe
Nick Doiron <nick.doiron@hpe.com>
2023-04-05 19:27:29 +0100

    experiment dropdown has right options and they do things

--- webui/react/src/pages/F_ExpList/F_ExperimentList.tsx
+++ webui/react/src/pages/F_ExpList/F_ExperimentList.tsx
@@ -128,6 +128,7 @@ const F_ExperimentList: React.FC<Props> = ({ project }) => {
             handleScroll={handleScroll}
             initialScrollPositionSet={initialScrollPositionSet}
             page={page}
+            project={project}
             selectAll={selectAll}
             selectedExperimentIds={selectedExperimentIds}
             setSelectAll={setSelectAll}
